5cc3adb7208e17b4fa4ab33a6dd2258c0943634c
kelly <kellymachado9797@gmail.com>
2024-09-30 22:24:32 -0300

    Login and register finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: LoginComponent, pathMatch: 'full' },
+  {path:'register', component: RegisterComponent, pathMatch:'prefix'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutes {
+
+}
