46a355d695afc7dbfa6ad765c5dad30e67b74b54
kelly <kellymachado9797@gmail.com>
2024-10-02 18:17:20 -0300

    Homepage content development

--- src/app/pages/index/home/home.component.ts
+++ src/app/pages/index/home/home.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from '../navigation/navigation.component';
+import { ChartData, ChartOptions } from 'chart.js/auto';
 
 @Component({
   selector: 'home',
   standalone: true,
-  imports: [],
+  imports: [ NavigationComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+    public doughnutChartData: ChartData<'doughnut'> = {
+      labels: ['Rent', 'Groceries', 'Entertainment'],
+      datasets: [
+        {
+          data: [500, 300, 200],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+        }
+      ]
+    };
+  
+    public doughnutChartOptions: ChartOptions<'doughnut'> = {
+      responsive: true,
+      cutout: '50%', // Define o centro oco
+      plugins: {
+        legend: {
+          position: 'top'
+        }
+      }
+    };
 
 }
--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import {  } from 'ng2-charts';
 
 @NgModule({
-  declarations: [],
+  declarations: [ ],
   imports: [
     CommonModule
   ]
