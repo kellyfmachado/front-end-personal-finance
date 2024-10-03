e7010a9f894d2b50e94b8bad01df8e1c54d8c0d7
kelly <kellymachado9797@gmail.com>
2024-10-02 21:51:56 -0300

    Adding graphs

--- src/app/pages/index/home/home.component.ts
+++ src/app/pages/index/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
-import { ChartData, ChartOptions } from 'chart.js/auto';
 
 @Component({
   selector: 'home',
@@ -10,26 +9,5 @@ import { ChartData, ChartOptions } from 'chart.js/auto';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
-    public doughnutChartData: ChartData<'doughnut'> = {
-      labels: ['Rent', 'Groceries', 'Entertainment'],
-      datasets: [
-        {
-          data: [500, 300, 200],
-          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-        }
-      ]
-    };
-  
-    public doughnutChartOptions: ChartOptions<'doughnut'> = {
-      responsive: true,
-      cutout: '50%', // Define o centro oco
-      plugins: {
-        legend: {
-          position: 'top'
-        }
-      }
-    };
 
 }
--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  } from 'ng2-charts';
 
 @NgModule({
   declarations: [ ],
