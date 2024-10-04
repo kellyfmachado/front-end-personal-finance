101c357ef5f44b9d6f167a491bfdc4b829872c52
kelly <kellymachado9797@gmail.com>
2024-10-04 00:49:20 -0300

    Adding user area

--- src/app/pages/index/navigation/navigation.component.ts
+++ src/app/pages/index/navigation/navigation.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'navigation',
   standalone: true,
-  imports: [],
+  imports: [ NgIf ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
 
+
+  isAlive: boolean = false;
+
+  public userBox () {
+    this.isAlive = !this.isAlive
+  }
 }
