0b388df7f05c1d95b8149ee82441aa6e92cb16ba
kelly <kellymachado9797@gmail.com>
2024-10-03 23:57:51 -0300

    Customizing graphs

--- src/app/pages/index/home/home.component.ts
+++ src/app/pages/index/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
-import { ChartOptions, ChartData } from 'chart.js';
+import { ChartOptions, ChartData, LineElement } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { isPlatformBrowser, NgIf} from '@angular/common';
 
@@ -18,30 +18,55 @@ export class HomeComponent {
     this.isBrowser = isPlatformBrowser(this.platformId);
   }
 
-  // Dados do gráfico
+  // Category graph data
   public doughnutChartData: ChartData<'doughnut'> = {
     labels: ['Rent', 'Groceries', 'Entertainment'],
     datasets: [
       {
-        data: [500, 300, 200],
+        data: [500, 300, 200, 100, 100, 50, 70],
         backgroundColor: ['#35a99b', '#f09a42', '#f38045'],
         hoverBackgroundColor: ['#62c7bb', '#eeb072', '#f3a178']
       }
     ]
   };
 
-  // Opções do gráfico
+  // Category graph options
   public doughnutChartOptions: ChartOptions<'doughnut'> = {
     responsive: true,
-    cutout: '80%',
+    cutout: '70%',
     plugins: {
       legend: {
         position: 'bottom',
         labels: {
-
+          color: '#ffffff',
+          font: {
+            family: 'Arial',
+            size: 20
+          },
+          pointStyle: 'circle',
+          usePointStyle: true
         }
       }
     }
+  };
+
+  //Amount graph data
+  public lineChartData: ChartData<'line'> = {
+    labels: ['item1', 'item2', 'item3', 'item4', 'item5', 'item6'],
+
+    datasets: [
+      {
+        data: [500, 300, 200, 250, 230, 350, 150, 420],
+        backgroundColor: ['#35a99b', '#f09a42', '#f38045'],
+        hoverBackgroundColor: ['#62c7bb', '#eeb072', '#f3a178'],
+
+      }
+    ]
+  };
+
+  // Amount graph options
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true
   };
 
 }
