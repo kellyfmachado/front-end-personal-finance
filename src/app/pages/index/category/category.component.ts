5cbca5ccf715d96b2cd3d252a1fde4ce6f97ce3f
kelly <kellymachado9797@gmail.com>
2024-10-09 15:39:44 -0300

    Transaction page finished

--- src/app/pages/index/category/category.component.ts
+++ src/app/pages/index/category/category.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
+import { CategoryListComponent } from '../category-list/category-list.component';
 
 @Component({
   selector: 'category',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, CategoryListComponent],
   templateUrl: './category.component.html',
   styleUrl: './category.component.css'
 })
--- src/app/pages/index/transaction/transaction.component.ts
+++ src/app/pages/index/transaction/transaction.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../navigation/navigation.component";
+import { TransactionListComponent } from '../transaction-list/transaction-list.component';
 
 @Component({
   selector: 'transaction',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, TransactionListComponent],
   templateUrl: './transaction.component.html',
   styleUrl: './transaction.component.css'
 })
